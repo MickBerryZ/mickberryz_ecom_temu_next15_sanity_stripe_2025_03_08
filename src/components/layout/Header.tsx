0c6f4585523f338e688406d412a6283da749e713
mickberryz <sa.thawatchai@gmail.com>
2025-05-11 17:26:51 +0100

    categorySelector Part 1

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -20,9 +20,10 @@ const AnnouncementBar = () => {
 
 type HeaderProps = {
   user: Omit<User, "passwordHash"> | null;
+  categorySelector: React.ReactNode;
 };
 
-const Header = ({ user }: HeaderProps) => {
+const Header = ({ user, categorySelector }: HeaderProps) => {
   const router = useRouter();
   const [isOpen, setIsOpen] = useState<boolean>(true);
   const [prevScrollY, setPrevScrollY] = useState<number>(0);
@@ -82,6 +83,7 @@ const Header = ({ user }: HeaderProps) => {
               <nav className="hidden md:flex gap-4 lg:gap-6 text-sm font-medium">
                 <Link href="#">Shop</Link>
                 <Link href="#">New Arrivals</Link>
+                {categorySelector}
                 <Link href="#">Sale</Link>
               </nav>
             </div>
