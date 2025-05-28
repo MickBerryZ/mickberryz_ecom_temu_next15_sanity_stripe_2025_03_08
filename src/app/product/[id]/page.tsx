5f842e3a97dea6688e40b39a2e3f176916b9e0e2
mickberryz <sa.thawatchai@gmail.com>
2025-05-28 19:29:49 +0100

    Show datails in ProductId & Setting layout

--- src/app/product/[id]/page.tsx
+++ src/app/product/[id]/page.tsx
@@ -1,9 +1,38 @@
+import SalesCampaignBanner from "@/components/layout/SalesCampaignBanner";
+import { getProductById } from "@/sanity/lib/client";
+import { Home } from "lucide-react";
+import Link from "next/link";
 import React from "react";
 
-const ProductPage = () => {
+const ProductPage = async ({ params }: { params: Promise<{ id: string }> }) => {
+  const { id } = await params;
+  const product = await getProductById(id);
+
+  if (!product.price) {
+    return <div>Product not found</div>;
+  }
+  const originalPrice = product.price * 5;
   return (
-    <div>
-      <div>ProductPage</div>
+    <div className="bg-gray-50">
+      <SalesCampaignBanner />
+
+      {/* Breadcrump Navigation */}
+      <div className="bg-white border-b border-gray-200">
+        <div className="container mx-auto py-3 px-4">
+          <Link
+            href="/"
+            className="text-gray-600 hover:text-red-600 transition-colors flex items-center gap-1"
+          >
+            <Home className="w-4 h-4" />
+            <span>Home</span>
+          </Link>
+        </div>
+      </div>
+
+      {/* Product Sale Banner */}
+      {/* Guarantee Items */}
+      {/* Product Details */}
+      <div>{JSON.stringify(product)}</div>
     </div>
   );
 };
