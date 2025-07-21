c7951e5581813324a850890279c5febd94e99299
mickberryz <sa.thawatchai@gmail.com>
2025-07-21 21:03:39 +0100

    1. create function updateCartItem 2. create revalidatePath("/")

--- src/actions/cart-actions.ts
+++ src/actions/cart-actions.ts
@@ -2,6 +2,7 @@
 
 import { getCurrentSession } from "@/actions/auth";
 import prisma from "@/lib/prisma";
+import { revalidatePath } from "next/cache";
 
 export const createCart = async () => {
   const { user } = await getCurrentSession();
@@ -58,4 +59,56 @@ export const getOrCreateCart = async (cartId?: string | null) => {
   }
 
   return cart;
+};
+
+export const updateCartItem = async (
+  cartId: string,
+  sanityProductId: string,
+  data: {
+    title?: string;
+    price?: number;
+    image?: string;
+    quantity?: number;
+  }
+) => {
+  const cart = await getOrCreateCart(cartId);
+
+  const existingItem = cart.items.find(
+    (item) => sanityProductId === item.sanityProductId
+  );
+
+  if (existingItem) {
+    // Update quantity
+    if (data.quantity === 0) {
+      await prisma.cartLineItem.delete({
+        where: {
+          id: existingItem.id,
+        },
+      });
+    } else if (data.quantity && data.quantity > 0) {
+      await prisma.cartLineItem.update({
+        where: {
+          id: existingItem.id,
+        },
+        data: {
+          quantity: data.quantity,
+        },
+      });
+    }
+  } else if (data.quantity && data.quantity > 0) {
+    await prisma.cartLineItem.create({
+      data: {
+        id: crypto.randomUUID(),
+        cartId: cart.id,
+        sanityProductId,
+        quantity: data.quantity || 1,
+        title: data.title || "",
+        price: data.price || 0,
+        image: data.image || "",
+      },
+    });
+  }
+
+  revalidatePath("/");
+  return getOrCreateCart(cartId);
 };
