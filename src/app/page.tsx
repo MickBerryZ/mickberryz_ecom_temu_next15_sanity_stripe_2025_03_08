83453214392e7f6c4db71675556153253db4a8b8
mickberryz <sa.thawatchai@gmail.com>
2026-01-04 19:08:15 +0000

    modiy lib/utils.ts || paste <Dialog>

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import { getCurrentSession } from "@/actions/auth";
 import SalesCampaignBanner from "@/components/layout/SalesCampaignBanner";
+import WheelOfFortune from "@/components/layout/WheelOfFortune";
 import ProductGrid from "@/components/product/ProductGrid";
 import { getAllProducts } from "@/sanity/lib/client";
 
@@ -11,6 +12,7 @@ const Home = async () => {
   return (
     <div>
       <SalesCampaignBanner />
+      <WheelOfFortune />
 
       <section className="container mx-auto py-8">
         <ProductGrid products={products} />
